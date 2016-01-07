98528b55abf2631d97b6f1069d1077470e2bbe60
Tina Sabine <purplecrayon95@hotmail.co.uk>
2016-01-07 12:35:29 +0000

    geocache

--- Balloons/ViewController.swift
+++ Balloons/ViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import MapKit
+import CoreLocation
 
 
 class ViewController: UIViewController {
@@ -8,11 +9,43 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var enterButton: UIButton!
  
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        
+        let bournemouthPier = CLLocationCoordinate2D(latitude: 50.716098, longitude: -1.875780)
+        
+        let bournemouthPierRegion = CLCircularRegion(center: bournemouthPier, radius: 50, identifier: "Bournemouth")
+        bournemouthPierRegion.notifyOnEntry = false
+        bournemouthPierRegion.notifyOnExit = false
+        locationManager.startMonitoringForRegion(bournemouthPierRegion)
+        
+        let boscombePier = CLLocationCoordinate2D(latitude: 50.719914, longitude: -1.843552)
+        
+        let boscombePierRegion = CLCircularRegion(center: boscombePier, radius: 50, identifier: "Boscombe")
+        boscombePierRegion.notifyOnEntry = false
+        boscombePierRegion.notifyOnExit = false
+        locationManager.startMonitoringForRegion(boscombePierRegion)
     }
-
-
-
+    
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("Entering \(region.identifier)")
+        
+       self.enterButton.hidden = false
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("Leaving \(region.identifier)")
+        
+        self.enterButton.hidden = true
+    }
+    
+}
